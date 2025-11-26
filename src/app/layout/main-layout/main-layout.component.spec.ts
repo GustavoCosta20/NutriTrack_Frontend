9127328c932e14b7015a28413ef89b755ce0bce2
Gustavo <gustavo.ghcosta2@gmail.com>
2025-11-25 22:55:54 -0300

    Task_022 - Correção da Sugestão IA

--- src/app/layout/main-layout/main-layout.component.spec.ts
+++ src/app/layout/main-layout/main-layout.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MainLayoutComponent } from './main-layout.component';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('MainLayoutComponent', () => {
   let component: MainLayoutComponent;
@@ -8,8 +8,10 @@ describe('MainLayoutComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [MainLayoutComponent]
+      declarations: [MainLayoutComponent],
+      imports: [RouterTestingModule]
     });
+
     fixture = TestBed.createComponent(MainLayoutComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
